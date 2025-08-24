d31b522149fc223398a0e7c0f8c7eb91a2c08bf8
Mushirul Hassan <mushirulhassan2@gmail.com>
2025-08-24 22:54:38 +0530

    fix: Correct production API URL

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -2,7 +2,7 @@ let IS_PROD = true;
 
 const server = IS_PROD ?
     
-     "https://https://conferabackend.onrender.com" :
+     "https://conferabackend.onrender.com" :
 
      "http://localhost:8000"
 
